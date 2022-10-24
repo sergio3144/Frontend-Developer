deee204cfd4d70af9f601d6721b7050348095336
Sergio Gonzalez <gonzalessergio3144@gmail.com>
2022-10-24 14:27:53 -0500

    cambios

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 
-const miFuncion = () => {
-    let container = document.querySelectorAll('.question-container');
-    container.forEach(element => {
-        let padre = element.children[0]
-        const res = element.children[1].children[0]
-        const flecha = element.children[0].children[1]
-        padre.addEventListener('click', (e) => {
-            flecha.style.transition = '360ms all';
-            flecha.classList.toggle('active-flecha')
-            res.classList.toggle('active-respuesta')
-        })
-    });
-}
-miFuncion()
+let container = document.querySelectorAll('.question-container');
+container.forEach((item, index) => {
+    let padre = item.children[0]
+    let hijo = item.children[1].children[0];
+    let flecha = item.children[0].children[1];
 
+    padre.addEventListener('click', () => {
+        flecha.style.transition = '360ms all';
+        flecha.classList.toggle('active-flecha');
+        hijo.classList.toggle('active-respuesta')
+    })
+})
 
 
 
